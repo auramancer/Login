2f66b61f23daf30af5a0632c770c7fb22dde0133
Feng Xiong <feng.xiong@mkodo.com>
2017-08-17 12:03:36 +0100

    Create project

---
+++ Login/Login/Login.h
@@ -0,0 +1,19 @@
+//
+//  Login.h
+//  Login
+//
+//  Created by fengx on 17/08/2017.
+//  Copyright © 2017 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Login.
+FOUNDATION_EXPORT double LoginVersionNumber;
+
+//! Project version string for Login.
+FOUNDATION_EXPORT const unsigned char LoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Login/PublicHeader.h>
+
+
